9ab8e6c7c3b046b081b71963528aac6fb70ed1d8
csarnel59300-a11y <csarnel59300@gmail.com>
2025-11-30 00:48:27 +0100

    Delete api/terms.js

--- api/terms.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'public', 'terms.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.status(200).send(html);
-}
