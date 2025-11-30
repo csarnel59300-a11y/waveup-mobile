05f38fbadc6f2f501aa5e8b87e107dd588c0cebb
csarnel59300-a11y <csarnel59300@gmail.com>
2025-11-30 01:57:46 +0100

    Delete api/privacy.js

--- api/privacy.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  const html = '<!DOCTYPE html><html lang="fr"><head><meta charset="UTF-8"><title>Politique de Confidentialité - WaveUp</title></head><body><h1>Politique de Confidentialité</h1><p>WaveUp respecte ta vie privée.</p></body></html>';
-  res.status(200).send(html);
-}
